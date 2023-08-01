55ee5d8674c56a4834e89dffc463d65d51ce12f9
arnob-rafi <arnobbkash@gmail.com>
2023-08-01 15:36:55 -0400

    manipulation file version 1

---
+++ version1/manipulating.h
@@ -0,0 +1,14 @@
+
+//Safeguarding the header file
+#ifndef _MANIPULATING_H
+#define _MANIPULATING_H
+
+//including the header files
+#include <stdio.h>
+#include <string.h>
+#define BUFFER_SIZE 100
+
+//function prototype is declared
+void manipulating();
+
+#endif
